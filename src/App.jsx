a52b50a918dd87ce446bae4005a863f44155ad83
Patricio Sirena <patobsirena@gmail.com>
2023-08-22 18:56:44 -0300

    Agregamos modulo de Manu

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNav from "./components/MyNav";
 import Footer from "./components/Footer";
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Menu from "./components/Menu";
 
 
 function App() {
@@ -14,48 +13,11 @@ function App() {
     return (
         <>
             <MyNav />
-            <CardGroup>
-                <Card className='m-4'>
-                    <Card.Img variant="top" src="" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card className='m-4'>
-                    <Card.Img variant="top" src="" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural lead-in to
-                            additional content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card className='m-4'>
-                    <Card.Img variant="top" src="" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in
-                            to additional content. This card has even longer content than the
-                            first to show that equal height action.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
+            <section className='d-flex justify-content-around'>
+            <Menu />
+            <Menu />
+            <Menu />
+            </section>
             <Footer className="footer" />
         </>
     )
