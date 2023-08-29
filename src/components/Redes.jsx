297fa5c394cf829f5792f8d122e6e09817719dd3
Patricio Sirena <patobsirena@gmail.com>
2023-08-29 00:23:53 -0300

    Agrego icono y listado a footer

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -1,19 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import logoPagina from "../assets/img/logo-pagina.jpg";
+
 
 const Redes = () => {
     return (
         <Container fluid className="footerRedes">
             <Row>
-                <Col className="p-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam facere reiciendis, porro laudantium aspernatur necessitatibus nemo recusandae enim provident illum earum doloremque. Cumque optio nemo voluptate dolor consequuntur, tenetur enim!
+                <Col className="p-5 mt-5 text-center accesoRedes">
+                    <h5>Seguinos en:</h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                 </Col>
-                <Col className="p-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam facere reiciendis, porro laudantium aspernatur necessitatibus nemo recusandae enim provident illum earum doloremque. Cumque optio nemo voluptate dolor consequuntur, tenetur enim!
+                <Col className="p-5 text-center">
+                    <img src={logoPagina} className="logoFooter" alt="" />
                 </Col>
-                <Col className="p-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam facere reiciendis, porro laudantium aspernatur necessitatibus nemo recusandae enim provident illum earum doloremque. Cumque optio nemo voluptate dolor consequuntur, tenetur enim!
+                <Col className="p-5 text-center listaFooter">
+                    <h5>Titulo de lista a ninguna parte</h5>
+                    <ul>
+                        <li>Link a ninguna parte</li>
+                        <li>Link a ninguna parte</li>
+                        <li>Link a ninguna parte</li>
+                    </ul>
                 </Col>
             </Row>
         </Container>
