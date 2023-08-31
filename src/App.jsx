ae1f193196e62c7a8dcedd0d1f48aa9a60ed33b5
Patricio Sirena <patobsirena@gmail.com>
2023-08-31 18:10:04 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 // import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/Footer";
@@ -7,14 +7,25 @@ import Cartas from "./components/Cartas";
 import Portada from "./components/Portada";
 import TarjetasInfo from "./components/TarjetasInfo";
 import MyNav from "./components/MyNav";
+import Axios from "axios";
 
+const rutaUsuarios = import.meta.env.VITE_ENV_USERS;
 
 function App() {
-
+    const [users, setUsers] = useState([])
+    const backEnd = async () => {
+        try {
+            const result = await Axios.get(rutaUsuarios + '/getUsers');
+            setUsers(result.data.usuarios);
+        } catch (err) {
+            return
+        }
+    }
     return (
         <>
             <MyNav />
             <Portada
+                users = {users}
                 titulo={'Titulo'}
                 descripccion={'soy una descripccion de prueba para ver si todo funciona bien! soy una descripccion de prueba para ver si todo funciona bien! soy una descripccion de prueba para ver si todo funciona bien!'} />
             <section className='d-flex justify-content-around'>
--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -2,20 +2,31 @@ import Button from 'react-bootstrap/Button';
 import portada from '../assets/img/portada.jpg'
 
 
-const Portada = ({ titulo, descripccion }) => {
+const Portada = ({ titulo, descripccion, users }) => {
+    const misUsers = users.map(user => <p>{user.email}</p>)
+
     return (
-        <div className="container-fluid">
-            <div className="row portada">
-                <div className="col-lg-8 portada-imagen">
-                    <img src={portada} alt="" />
-                </div>
-                <div className="col-lg-4 mb-5 portada-descripccion">
-                    <h1 className='mt-3'>{ titulo }</h1>
-                    <p className="mb-5"> { descripccion } </p>
-                    <Button className='boton-portada mt-5' variant="outline-dark">Dark</Button>
+        <>
+            <div className="container-fluid">
+                <div className="row portada">
+                    <div className="col-lg-8 portada-imagen">
+                        <img src={portada} alt="" />
+                    </div>
+                    <div className="col-lg-4 mb-5 portada-descripccion">
+                        <h1 className='mt-3'>{titulo}</h1>
+                        <p className="mb-5"> {descripccion} </p>
+                        <Button className='boton-portada mt-5' variant="outline-dark">Dark</Button>
+
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div>
+                {
+                    misUsers
+                }
+            </div>
+        </>
     );
 }
 
