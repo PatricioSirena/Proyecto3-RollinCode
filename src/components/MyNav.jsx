eab666fbce11d2857e894448157cdd9e9b99fb93
TuttySantochi <tuttysantochi69@gmail.com>
2023-08-31 21:48:49 -0300

    Creo Rama Admin
    Creo rama y Pagin admin, estructura basica

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,21 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Admin from '../pages/Admin'
+
 
 function MyNav() {
     return (
-        <>
+        <BrowserRouter>
             <Navbar className='navbar' bg="dark" data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Link to="/Admin">Home</Link>
+                        <Nav.Link href="">Admin</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav>
+                    
                 </Container>
+                <Routes>
+                <Route  path="/Admin" element={<Admin/>} />
+                </Routes>
+
             </Navbar>
-        </>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,11 @@
+import MyNav from "../components/MyNav"
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>Lo que se apor aqui</h1>
+    </div>
+  )
+}
+
+export default Admin
