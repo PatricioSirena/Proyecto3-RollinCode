7648680aee77d4248c210ac5ca0a5ad76f5d470e
Patricio Sirena <patobsirena@gmail.com>
2023-09-02 19:48:47 -0300

    .

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,155 @@
-
+import Button from 'react-bootstrap/Button';
 
 const Admin = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="productos text-center">
+        <h2>Productos</h2>
+        <div className='d-flex justify-content-end'>
+        <Button variant="dark">Agregar</Button>
+        </div>
+        <h3>Comidas</h3>
+        <ul className='listaComida'>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+                    <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+        </ul>
+        <hr />
+        <h3>Bebidas</h3>
+        <ul className='listaBebida'>
+        <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+        </ul>
+        <hr />
+        <h3>Flores</h3>
+        <ul className='listaFlores'>
+        <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+        </ul>
+      </section>
+      <br />
+      <section className='pedidos mt-5 text-center'>
+        <h3>Pedidos</h3>
+        <ul>
+        <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+                    <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+          <li className='d-flex justify-content-around m-3'>
+            <p>Nombre Prudcuto</p>
+            <p>Estado</p>
+            <Button variant="dark">Dark</Button>
+            <Button variant="dark">Dark</Button>
+          </li>
+        </ul>
+      </section>
+      <section className='usuarios'>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </section>
+    </>
   )
 }
 
