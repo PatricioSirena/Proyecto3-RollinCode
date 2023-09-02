7c7cf8f010f3a80fc45114c04af2e2b4df1ec0c4
Valentina Cáceres Mendoza <caceresmendoza.valentina@gmail.com>
2023-09-02 17:18:28 -0300

    agrego cambios en el nav

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,22 +1,51 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function MyNav() {
-    return (
-        <>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+      <Container fluid>
+        
+        <img
+                    src="./img/CFE972CF-072F-4524-95F2-ACEA6156465E.PNG"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="React Bootstrap logo"
+                />
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Iniciar Sesión</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  );
 }
 
 export default MyNav;
+
+// <Navbar.Brand href="#home">Navbar</Navbar.Brand>
