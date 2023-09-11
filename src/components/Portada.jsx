25b116ff7e7801fe3090ba07873a943ca5473379
Valentina Cáceres Mendoza <caceresmendoza.valentina@gmail.com>
2023-09-11 20:36:10 -0300

    cambios en portada.jsx

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import { Row, Col, Container } from 'react-bootstrap';
 import Proptypes from 'prop-types'
-import LogoYunga from '../img/logo_yungannabis.jpeg.jpg'
+import HappyBurger from '../img/portada_burger.jpg'
 import '../styles/portada.css'
 
 
@@ -10,11 +10,11 @@ const Portada = ({ titulo, descripccion }) => {
         <Container fluid className='imagenPortada'>
             <Row>
                 <Col lg={6} className='logoPortada text-center'>
-                    <img src={LogoYunga} width={'300px'} alt="" />
+                    <img className="imagen_portada_logo" src={HappyBurger} width={'1000px'} alt="" />
                 </Col>
                 <Col lg={6} className="mb-5 text-center">
                     <h1 className='tituloo text-center mt-3 animate__zoomInLeft'>{titulo}</h1>
-                    <p className="text-center mb-5"> {descripccion} </p>
+                    <p className="txt-portada text-center mb-5"> {descripccion} </p>
                     <Button className='boton-portada mt-5' variant="outline-light">Click</Button>
                 </Col>
             </Row>
