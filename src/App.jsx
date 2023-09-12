b127253d34c1077df1a1fc568ef4bc1abd9b35c9
solheredia <solheredia.555@gmail.com>
2023-09-11 21:11:48 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from 'react'
+
+// TEST
+
+
+// import { useState } from 'react'
 // import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Axios from "axios";
+// import Axios from "axios";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,28 +13,28 @@ import Home from './pages/Home';
 import Administrador from './pages/Administrador';
 
 
-const rutaUsuarios = import.meta.env.VITE_ENV_USERS;
+// const rutaUsuarios = import.meta.env.VITE_ENV_USERS;
 
 function App() {
-    const [users, setUsers] = useState([])
-    const backEnd = async () => {
-        try {
-            const result = await Axios.get(rutaUsuarios + '/getUsers');
-            setUsers(result.data.usuarios);
-        } catch (err) {
-            return
-        }
-    }
+    // const [users, setUsers] = useState([])
+    // const backEnd = async () => {
+    //     try {
+    //         const result = await Axios.get(rutaUsuarios + '/getUsers');
+    //         setUsers(result.data.usuarios);
+    //     } catch (err) {
+    //         return
+    //     }
+    // }
     return (
         <>
             <BrowserRouter>
                 <MyNav />
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route exact path="/admin" element={<Administrador />} />
-                        {/* <Route exact path="/login" element={<Admin />} /> */}
-                        {/* <Route exact path="/registro" element={<Admin />} /> */}
-                    </Routes>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/admin" element={<Administrador />} />
+                    {/* <Route exact path="/login" element={<Admin />} /> */}
+                    {/* <Route exact path="/registro" element={<Admin />} /> */}
+                </Routes>
             </BrowserRouter>
             {/* <Home /> */}
         </>
