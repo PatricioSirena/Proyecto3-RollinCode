b15c4c91dc7ddcabcbd61fae6a2e19582ea24348
TuttySantochi <tuttysantochi69@gmail.com>
2023-09-14 17:26:22 -0300

    Update Login.jsx
    borro cntenido innecesario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import '../styles/login.css'
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
 
