300c5e4a799d68b70b37c6e7225e8ca022a2744b
Patricio Sirena <patobsirena@gmail.com>
2023-09-14 18:15:36 -0300

    Update Nav

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -3,18 +3,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Happyfood from "../img/happy.jpg";
 import Carrito from "../img/carritobyn.jpg";
-//import Carita from "../img/carita.jpg";
 import '../styles/mynav.css'
 
 function MyNav() {
   return (
     <>
-      <div class="warning">
-          <div class="warning__icon">
+      <div className="warning">
+          <div className="warning__icon">
 
           </div>
-          <div class="warning__title">☻ Todos los Jueves 20% de Descuento en TODOS Nuestros Productos | Cod: BIENVENIDA_HAPPY</div>
-          <div class="warning__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
+          <div className="warning__title">☻ Todos los Jueves 20% de Descuento en TODOS Nuestros Productos | Cod: BIENVENIDA_HAPPY</div>
+          <div className="warning__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
       </div>
     <Navbar expand="lg" sticky="top" className="bg-body-tertiary" id="nav" data-bs-theme="light" roundedCircle>
       <Container fluid>
