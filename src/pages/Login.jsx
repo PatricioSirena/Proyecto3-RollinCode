a23fa72eb0fbad75baeda18e80c7e887380392a3
Valentina Cáceres Mendoza <caceresmendoza.valentina@gmail.com>
2023-09-14 01:31:32 -0300

    cambios en el button de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -99,68 +99,8 @@ const Login = () => {
 export default Login
 
 
-/*<input type="checkbox" id="flip"></input>
-                        <Row>
-                            <Col className="cover">
-                                <Col className="front">
-                                <img className='h-25' src={imagen} alt="" />
-                                <Col className="text">
-                                <span className="text-1">Bienvenido! <br /> A Happy Food</span>
-                                <span className="text-2">Conectémonos</span>
-                            </Col>  
-                        </Col>
-                    </Col>
-                    <Col className="forms">
-                        <Form className='formulario text-center'>
-                            <div>
-                                <label htmlFor="usuario">Usuario</label>
-                                <br />
-                                <Field
-                                    id="usuario"
-                                    type="text"
-                                    name="usuario"
-                                    placeholder="Introduce tu usuario"
-                                />
-                                <ErrorMessage name="usuario" component={() => (
-                                    <div className="error">{errors.usuario}</div>
-                                )} />
-                            </div>
-                            <div>
-                                <label htmlFor="contraseña">Contraseña</label>
-                                <br />
-                                <Field
-                                    id="contraseña"
-                                    type="password"
-                                    name="contraseña"
-                                    placeholder="Introduce tu contraseña"
-                                />
-                                <ErrorMessage name="contraseña" component={() => (
-                                    <div className="error">{errors.contraseña}</div>
-                                )} />
-                            </div>
-                            <button type="submit" className="btn btn-dark mt-3 mb-5 w-50" >Iniciar Sesión</button>
-                            {datosEnviados && <p className="usuarioRegistrado">Te registraste con exito!</p>}
-                            <div className='d-flex justify-content-center'>
-                                <a href="#!"><p>Has olvidado tu contraseña?</p></a>
-                            </div>
-                        </Form>
-                    </Col>
-                </Row>
+
                 
                 
                 
-                 {/* <input type="submit" value="Login" class="btn solid" />
-                                            <p className="social-text">Or Sign in with social platforms</p>
-                                            <Col className="social-media">
-                                                <a href="#" class="social-icon">
-                                                    <i class="fab fa-facebook-f"></i>
-                                                </a>
-                                                <a href="#" class="social-icon">
-                                                    <i class="fab fa-twitter"></i>
-                                                </a>
-                                                <a href="#" class="social-icon">
-                                                    <i class="fab fa-google"></i>
-                                                </a>
-                                                <a href="#" class="social-icon">
-                                                    <i class="fab fa-linkedin-in"></i>
-                                            </a>*/
+                 
