dfe3a859e360fc2517a24483caa61c917616f317
Patricio Sirena <patobsirena@gmail.com>
2023-09-29 17:43:35 -0300

    Modifico tabla en admin

--- src/pages/Administrador.jsx
+++ src/pages/Administrador.jsx
@@ -1,186 +1,118 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { show_alerta } from '../helpers/alerta';
+import { useForm } from 'react-hook-form'
+import '../styles/administrador.css'
 
 const Administrador = () => {
-    const url="http://localhost:3000/menu";
-    const [products,setProducts]= useState([]);
-    const [id,setId]= useState('');
-    const [name,setName]= useState('');
-    const [description,setDescription]= useState('');
-    const [price,setPrice]= useState('');
-    const [operation,setOperation]= useState(0);
-    const [title,setTitle]= useState('');
 
-    useEffect( ()=>{
-        getProducts();
-    },[]);
+    const { formState: { errors }, handleSubmit, register, reset } = useForm();
 
-    const getProducts = async () => {
-        const respuesta = await axios.get(url);
-        setProducts(respuesta.data);
-    }
-    const openModal = (op,id, name, description, price) =>{
-        setId('');
-        setName('');
-        setDescription('');
-        setPrice('');
-        setOperation(op);
-        if(op === 1){
-            setTitle('Registrar Producto');
-        }
-        else if(op === 2){
-            setTitle('Editar Producto');
-            setId(id);
-            setName(name);
-            setDescription(description);
-            setPrice(price);
-        }
-        window.setTimeout(() => {
-            document.getElementById('nombre').focus();
-        },500);
-    }
-    const validar = () => {
-        var parametros;
-        var metodo;
-        if(name.trim() === ''){
-            show_alerta('Escribe el nombre del producto','warning');
-        }
-        else if(description.trim() === ''){
-            show_alerta('Escribe la descripción del producto','warning');
-        }
-        else if(price === ''){
-            show_alerta('Escribe el precio del producto','warning');
-        }
-        else{
-            if(operation === 1){
-                parametros= {name:name.trim(),description: description.trim(),price:price};
-                metodo= 'POST';
-            }
-            else{
-                parametros={id:id,name:name.trim(),description: description.trim(),price:price};
-                metodo= 'PUT';
-            }
-            envarSolicitud(metodo,parametros);
-        }
-    }
-    const envarSolicitud = async(metodo,parametros) => {
-        await axios({ method:metodo, url: url, data:parametros}).then(function(respuesta){
-            var tipo = respuesta.data[0];
-            var msj = respuesta.data[1];
-            show_alerta(msj,tipo);
-            if(tipo === 'success'){
-                document.getElementById('btnCerrar').click();
-                getProducts();
-            }
-        })
-        .catch(function(error){
-            show_alerta('Error en la solicitud','error');
-            console.log(error);
-        });
-    }
-    const deleteProduct= (id,name) =>{
-        const MySwal = withReactContent(Swal);
-        MySwal.fire({
-            title:'¿Seguro de eliminar el producto '+name+' ?',
-            icon: 'question',text:'No se podrá dar marcha atrás',
-            showCancelButton:true,confirmButtonText:'Si, eliminar',cancelButtonText:'Cancelar'
-        }).then((result) =>{
-            if(result.isConfirmed){
-                setId(id);
-                envarSolicitud('DELETE',{id:id});
-            }
-            else{
-                show_alerta('El producto NO fue eliminado','info');
-            }
-        });
-    }
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+    })
 
-  return (
-    <div className='App'>
-        <div className='container-fluid'>
-            <div className='row mt-3'>
-                <div className='col-md-4 offset-md-4'>
-                    <div className='d-grid mx-auto'>
-                        <button onClick={()=> openModal(1)} className='btn btn-dark' data-bs-toggle='modal' data-bs-target='#modalProducts'>
-                            <i className='fa-solid fa-circle-plus'></i> Añadir
-                        </button>
+    return (
+        <>
+            <div className='agregarPlato'>
+                <form onSubmit={onSubmit}>
+                    {/* {Nombre} */}
+                    <label htmlFor='titulo'>Nombre</label>
+                    <input type="text"
+                        {...register('titulo', {
+                            required: {
+                                value: true,
+                                message: "El nombre es requerido"
+                            },
+                            pattern: {
+                                value: /^[A-Za-z0-9]{4,20}\S+$/,
+                                message: 'Solo letras y numeros son validos.'
+                            }
+                        }
+                        )}
+                    />
+                    errors.titulo && <span>{errors.titulo.message}</span>
+                    
+                    {/* {Precio} */}
+                    <label htmlFor="precio">Precio</label>
+                    <input type="number"
+                        {...register('precio', {
+                            required: {
+                                value: true,
+                                message: "El precio es requerido"
+                            },
+                            pattern: {
+                                value: /^[0-9]+[.,]{1,1}\[0]{2,2}$/,
+                                message: 'Debe ingresar un número con dos decimales y no debe contener ningún otro símbolo.'
+                            }
+                        }
+                        )}
+                    />
+                    errors.precio && <span>{errors.precio.message}</span>
+                    {/* {Descripcion} */}
+                    <label htmlFor="texto">Descripción</label>
+                    <input type="text"
+                        {...register('texto', {
+                            required: {
+                                value: true,
+                                message: "La descripción es requerida"
+                            },
+                            pattern: {
+
+                            }
+                        }
+                        )}
+                    />
+
+                    {/* {Categoria} */}
+                    <label htmlFor="categoria">Categoria</label>
+                    <select
+                        {...register('categoria', {
+                            required: {
+                                value: true,
+                                message: "La categoria es requerida"
+                            },
+                            pattern: {
+
+                            }
+                        }
+                        )}
+                    >
+                        <option value="comida">Comida</option>
+                        <option value="bebida">Bebida</option>
+                        <option value="flor">Flor</option>
+                    </select>
+
+                    {/* {imagen} */}
+                    <label htmlFor="imagen">URL Imagen</label>
+                    <input type="text"
+                        {...register('imagen', {
+                            required: {
+                                value: true,
+                                message: "La imagen es requerida"
+                            },
+                            pattern: {
+
+                            }
+                        }
+                        )}
+                    />
+
+                    {/* {Activo} */}
+                    <div className='platoActivo'>
+                        <label htmlFor="activo">Publicar Plato</label>
+                        <input type="checkbox"
+                            {...register('activo')}
+                        />
                     </div>
-                </div>
+
+                    <div className='botones'>
+                        <button className='btn btn-dark' type='submit'>Guardar</button>
+                        <button className='btn btn-dark'>Cancelar</button>
+
+                    </div>
+
+                </form>
             </div>
-            <div className='row mt-3'>
-                <div className='col-12 col-lg-8 offset-0 offset-lg-2'>
-                    <div className='table-responsive'>
-                        <table className='table table-bordered'>
-                            <thead>
-                                <tr><th>#</th><th>PRODUCTO</th><th>DESCRIPCION</th><th>PRECIO</th><th></th></tr>
-                            </thead>
-                            <tbody className='table-group-divider'>
-                                {products.map( (product,i)=>(
-                                    <tr key={product.id}>
-                                        <td>{(i+1)}</td>
-                                        <td>{product.name}</td>
-                                        <td>{product.description}</td>
-                                        <td>${new Intl.NumberFormat('es-mx').format(product.price)}</td>
-                                        <td>
-                                            <button onClick={() => openModal(2,product.id,product.name,product.description,product.price)}
-                                                 className='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modalProducts'>
-                                                <i className='fa-solid fa-edit'></i>
-                                            </button>
-                                            &nbsp; 
-                                            <button onClick={()=>deleteProduct(product.id,product.name)} className='btn btn-danger'>
-                                                <i className='fa-solid fa-trash'></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id='modalProducts' className='modal fade' aria-hidden='true'>
-            <div className='modal-dialog'>
-                <div className='modal-content'>
-                    <div className='modal-header'>
-                        <label className='h5'>{title}</label>
-                        <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                    </div>
-                    <div className='modal-body'>
-                        <input type='hidden' id='id'></input>
-                        <div className='input-group mb-3'>
-                            <span className='input-group-text'><i className='fa-solid fa-gift'></i></span>
-                            <input type='text' id='nombre' className='form-control' placeholder='Nombre' value={name}
-                            onChange={(e)=> setName(e.target.value)}></input>
-                        </div>
-                        <div className='input-group mb-3'>
-                            <span className='input-group-text'><i className='fa-solid fa-comment'></i></span>
-                            <input type='text' id='descripcion' className='form-control' placeholder='Descripción' value={description}
-                            onChange={(e)=> setDescription(e.target.value)}></input>
-                        </div>
-                        <div className='input-group mb-3'>
-                            <span className='input-group-text'><i className='fa-solid fa-dollar-sign'></i></span>
-                            <input type='text' id='precio' className='form-control' placeholder='Precio' value={price}
-                            onChange={(e)=> setPrice(e.target.value)}></input>
-                        </div>
-                        <div className='d-grid col-6 mx-auto'>
-                            <button onClick={() => validar()} className='btn btn-success'>
-                                <i className='fa-solid fa-floppy-disk'></i> Guardar
-                            </button>
-                        </div>
-                    </div>
-                    <div className='modal-footer'>
-                        <button type='button' id='btnCerrar' className='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+        </>
+    )
 }
 
 export default Administrador
