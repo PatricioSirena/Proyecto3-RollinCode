a5e68c4303308071bc25f2ad0b51312a87d86d8a
nanosp7 <spectormariano1@gmail.com>
2023-11-05 16:22:26 -0300

    error404

---
+++ src/pages/error404.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+
+function error404() {
+    return (
+        <>
+            <Footer className="footer" />
+        </>
+    )
+}
+
+export default error404
