ebb9cc0d8efedb7decf91e8da5ab0d9246ee6a75
Patricio Sirena <patobsirena@gmail.com>
2023-11-09 17:47:41 -0300

    Eliminando codigo innecesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import { useState } from 'react'
-// import Axios from "axios";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,19 +5,8 @@ import MyNav from "./components/MyNav";
 import Home from './pages/Home';
 import Administrador from './pages/Administrador';
 
-
-// const rutaUsuarios = import.meta.env.VITE_ENV_USERS;
-
 function App() {
-    // const [users, setUsers] = useState([])
-    // const backEnd = async () => {
-    //     try {
-    //         const result = await Axios.get(rutaUsuarios + '/getUsers');
-    //         setUsers(result.data.usuarios);
-    //     } catch (err) {
-    //         return
-    //     }
-    // }
+
     return (
         <>
             <BrowserRouter>
@@ -27,11 +14,8 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Home />} />
                         <Route exact path="/admin" element={<Administrador />} />
-                        {/* <Route exact path="/login" element={<Admin />} /> */}
-                        {/* <Route exact path="/registro" element={<Admin />} /> */}
                     </Routes>
             </BrowserRouter>
-            {/* <Home /> */}
         </>
     )
 }
