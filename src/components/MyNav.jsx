2a89855ec65e14d07906caaff40fce5043e04d25
Patricio Sirena <patobsirena@gmail.com>
2023-12-08 15:41:31 -0300

    Modifico y agrego medidas para responsive de nav y portada

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -85,11 +85,9 @@ function MyNav({ isLogueado, setIslogueado, admin, setAdmin, setUser }) {
     return (
         <>
             <div className="warning">
-                <div className="warning__title">
-                    <span className='spann'>
+                    <span className='ofertaNav'>
                         ☻ Todos los Jueves 20% de Descuento en TODOS Nuestros Productos | Cod: BIENVENIDA_HAPPY
                     </span>
-                </div>
                 <div className="warning__close"><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z" fill="#393a37"></path></svg></div>
             </div>
             <Navbar expand="lg" sticky="top" className="bg-body-tertiary" id="nav" data-bs-theme="light">
