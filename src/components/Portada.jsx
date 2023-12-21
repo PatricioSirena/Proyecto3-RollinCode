8f49f88686d35607eecf6846df8a787bada90c6e
Patricio Sirena <patobsirena@gmail.com>
2023-12-21 14:16:33 -0300

    Modifico responsive de portada

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -15,10 +15,10 @@ const Portada = ({ titulo, descripccion }) => {
                         <img className="imagen_portada_logo" src={HappyBurger} alt="" />
                     </Col>
                     <Col lg={7} className="container-txt text-center">
-                        <img className="imagen_portada_yunga" src={LogoYunga} width={'400px'} alt="" />
+                        <img className="imagen_portada_yunga" src={LogoYunga} alt="" />
                         <br />
-                        <h1 className='titulo-portada text-center mb-4 animate__backInRight'>{titulo}</h1>
-                        <p className="txt-portada text-center mb-5"> {descripccion} </p>
+                        <h1 className='titulo-portada mb-4 animate__backInRight'>{titulo}</h1>
+                        <p className="txt-portada mb-5"> {descripccion} </p>
                         <Button className='boton-portada mt-2' variant="outline-light">
                         <a href="https://www.fundacionyungannabis.org.ar/" target='_blank'>Conocenos</a>
                         </Button>
